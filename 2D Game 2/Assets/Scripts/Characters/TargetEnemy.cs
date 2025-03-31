766a1275bb1e11bd2ff4996b71b2a460f9d10f1c
threeboysinsideatrenchcoat <157040871+threeboysinsideatrenchcoat@users.noreply.github.com>
2025-03-31 18:29:07 -0400

    save

---
+++ 2D Game 2/Assets/Scripts/Characters/TargetEnemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetEnemy : Enemy
+{
+    public GameObject targetTower; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetTower = GameObject.FindWithTag("Tower");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayHealth();
+        manager = FindObjectOfType<GameManager>();
+        thisObject.transform.position += VectorToTower() + speed; 
+    }
+
+    Vector3 VectorToTower()
+    {
+        Vector3 targetDirecton;
+        targetDirection = targetTower.transform.position - transform.position; 
+        targetDirection = targetDirection.normalized; 
+
+        return targetDirection; 
+    }
+}
